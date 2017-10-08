9cb644e1d7a6b7f8dcc175693717dd02fc461430
berbegaa <agustin.berbegall@roche.com>
2017-10-08 13:16:22 +0200

    Refactored code to support multiple connections based on a given path

--- other/js/websockify.js
+++ other/js/websockify.js
@@ -8,7 +8,6 @@
 // Requires node modules: ws and optimist
 //     npm install ws optimist
 
-
 var argv = require('optimist').argv,
     net = require('net'),
     http = require('http'),
@@ -16,85 +15,122 @@ var argv = require('optimist').argv,
     url = require('url'),
     path = require('path'),
     fs = require('fs'),
-
-    Buffer = require('buffer').Buffer,
+    tunnel = require('tunnel-ssh'),
     WebSocketServer = require('ws').Server,
+    sshtunnel,
+    sshkey;
 
-    webServer, wsServer,
-    source_host, source_port, target_host, target_port,
-    web_path = null;
+const source_port = 5995;
+const log = function (client, msg) {
+    console.log(client + ': ' + msg);
+};
 
+const connect_tunnel = function (client, req) {
+    var clientAddr = client._socket.remoteAddress;
+    var requested_url = req ? req.url : client.upgradeReq.url;
+    var url_parts = url.parse(requested_url, true);
+    var target_host = url_parts.query.targethost;
+    const target_port = 5900;
+    const sshuser = 'user';
+    const sshkeypass = 'passphrase';
+    const sshlocalport = 0;
+
+    log(clientAddr, 'Tunnel connection...');
+    log(clientAddr, 'Request: ' + requested_url);
+
+    const config = {
+        username: sshuser,
+        privateKey: sshkey,
+        passphrase: sshkeypass,
+        host: target_host,
+        dstPort: target_port,
+        localPort: sshlocalport
+    };
+    sshtunnel = tunnel(config, function (error, server) {
+        if (error) {
+            log(clientAddr, 'Error found when configuring ssh tunnel');
+        }
+        else {
+            log(clientAddr, 'Tunneling target connection: ' + target_host + ':' + target_port);
+        }
+    });
+    sshtunnel.on('listening', function () {
+        var target_port_redirected = sshtunnel.address().port;
+        var target_host_redirected = sshtunnel.address().address;
+        log(clientAddr, 'Listening on ssh tunnel: ' + target_host_redirected + ':' + target_port_redirected);
+        new_client(client, req, clientAddr, target_host_redirected, target_port_redirected);
+    });
+    sshtunnel.on('error', function (err) {
+        console.error('Error found on ssh tunnel:', err);
+        sshtunnel.close();
+    });
+}
 
 // Handle new WebSocket client
-new_client = function(client, req) {
-    var clientAddr = client._socket.remoteAddress, log;
-    console.log(req ? req.url : client.upgradeReq.url);
-    log = function (msg) {
-        console.log(' ' + clientAddr + ': '+ msg);
-    };
-    log('WebSocket connection');
-    log('Version ' + client.protocolVersion + ', subprotocol: ' + client.protocol);
+const new_client = function (client, req, clientAddr, target_host, target_port) {
+    log(clientAddr, 'WebSocket connection...');
+    log(clientAddr, 'Version ' + client.protocolVersion + ', subprotocol: ' + client.protocol);
 
-    var target = net.createConnection(target_port,target_host, function() {
-        log('connected to target');
+    var target = net.createConnection(target_port, target_host, function () {
+        log(clientAddr, 'Connected to target: ' + target_host + ":" + target_port);
     });
-    target.on('data', function(data) {
-        //log("sending message: " + data);
+
+    target.on('data', function (data) {
         try {
             client.send(data);
-        } catch(e) {
-            log("Client closed, cleaning up target");
+        }
+        catch (e) {
+            log(clientAddr, 'Client closed, cleaning up target');
             target.end();
         }
     });
-    target.on('end', function() {
-        log('target disconnected');
+    target.on('end', function () {
+        log(clientAddr, 'target disconnected');
         client.close();
+        sshtunnel.close();
     });
-    target.on('error', function() {
-        log('target connection error');
+    target.on('error', function () {
+        log(clientAddr, 'target connection error');
         target.end();
         client.close();
+        sshtunnel.close();
     });
 
-    client.on('message', function(msg) {
-        //log('got message: ' + msg);
+    client.on('message', function (msg) {
         target.write(msg);
     });
-    client.on('close', function(code, reason) {
-        log('WebSocket client disconnected: ' + code + ' [' + reason + ']');
+    client.on('close', function (code, reason) {
+        log(clientAddr, 'WebSocket client disconnected: ' + code + ' [' + reason + ']');
         target.end();
+        sshtunnel.close();
     });
-    client.on('error', function(a) {
-        log('WebSocket client error: ' + a);
+    client.on('error', function (a) {
+        log(clientAddr, 'WebSocket client error: ' + a);
         target.end();
+        sshtunnel.close();
     });
 };
 
-
 // Send an HTTP error response
-http_error = function (response, code, msg) {
-    response.writeHead(code, {"Content-Type": "text/plain"});
+const http_error = function (response, code, msg) {
+    response.writeHead(code, { "Content-Type": "text/plain" });
     response.write(msg + "\n");
     response.end();
     return;
 }
 
 // Process an HTTP static file request
-http_request = function (request, response) {
-//    console.log("pathname: " + url.parse(req.url).pathname);
-//    res.writeHead(200, {'Content-Type': 'text/plain'});
-//    res.end('okay');
+const http_request = function (request, response) {
+    console.log("An http request has arrived: " + request.url);
 
-    if (! argv.web) {
+    if (!argv.web) {
         return http_error(response, 403, "403 Permission Denied");
     }
 
-    var uri = url.parse(request.url).pathname
-        , filename = path.join(argv.web, uri);
+    var uri = url.parse(request.url).pathname, filename = path.join(argv.web, uri);
 
-    fs.exists(filename, function(exists) {
-        if(!exists) {
+    fs.exists(filename, function (exists) {
+        if (!exists) {
             return http_error(response, 404, "404 Not Found");
         }
 
@@ -102,8 +138,8 @@ http_request = function (request, response) {
             filename += '/index.html';
         }
 
-        fs.readFile(filename, "binary", function(err, file) {
-            if(err) {
+        fs.readFile(filename, "binary", function (err, file) {
+            if (err) {
                 return http_error(response, 500, err);
             }
 
@@ -114,54 +150,14 @@ http_request = function (request, response) {
     });
 };
 
-// parse source and target arguments into parts
-try {
-    source_arg = argv._[0].toString();
-    target_arg = argv._[1].toString();
+console.log("- Running in unencrypted HTTP (ws://) mode");
+var webServer = http.createServer(http_request);
 
-    var idx;
-    idx = source_arg.indexOf(":");
-    if (idx >= 0) {
-        source_host = source_arg.slice(0, idx);
-        source_port = parseInt(source_arg.slice(idx+1), 10);
-    } else {
-        source_host = "";
-        source_port = parseInt(source_arg, 10);
-    }
+console.log("- Target TCP connection will be redirected through a secure ssh channel");
+sshkey = fs.readFileSync('key.ppk');
 
-    idx = target_arg.indexOf(":");
-    if (idx < 0) {
-        throw("target must be host:port");
-    }
-    target_host = target_arg.slice(0, idx);
-    target_port = parseInt(target_arg.slice(idx+1), 10);
-
-    if (isNaN(source_port) || isNaN(target_port)) {
-        throw("illegal port");
-    }
-} catch(e) {
-    console.error("websockify.js [--web web_dir] [--cert cert.pem [--key key.pem]] [source_addr:]source_port target_addr:target_port");
-    process.exit(2);
-}
-
-console.log("WebSocket settings: ");
-console.log("    - proxying from " + source_host + ":" + source_port +
-            " to " + target_host + ":" + target_port);
-if (argv.web) {
-    console.log("    - Web server active. Serving: " + argv.web);
-}
-
-if (argv.cert) {
-    argv.key = argv.key || argv.cert;
-    var cert = fs.readFileSync(argv.cert),
-        key = fs.readFileSync(argv.key);
-    console.log("    - Running in encrypted HTTPS (wss://) mode using: " + argv.cert + ", " + argv.key);
-    webServer = https.createServer({cert: cert, key: key}, http_request);
-} else {
-    console.log("    - Running in unencrypted HTTP (ws://) mode");
-    webServer = http.createServer(http_request);
-}
-webServer.listen(source_port, function() {
-    wsServer = new WebSocketServer({server: webServer});
-    wsServer.on('connection', new_client);
+webServer.listen(source_port, function () {
+    console.log("- Listening on port " + source_port + " for HTTP request ...")
+    var wsServer = new WebSocketServer({ server: webServer });
+    wsServer.on('connection', connect_tunnel);
 });
